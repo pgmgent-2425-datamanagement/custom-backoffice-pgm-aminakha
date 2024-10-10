31e187f8848834d1b4fee5f044591c9d6d63996e
pgm-aminakha <aminakha@student.arteveldehs.be>
2024-10-10 15:09:36 +0200

    git init

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -2,12 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Models\Exercise;
+
 class HomeController extends BaseController {
 
     public static function index () {
+        $exercises = Exercise::all();
+      
+      
 
         self::loadView('/home', [
-            'title' => 'Homepage'
+            'title' => 'Homepage',
+            'exercises' => $exercises
         ]);
     }
 
--- views/home.php
+++ views/home.php
@@ -1,2 +1,12 @@
-<h1>Base MVC</h1>
+
+<h1>Basic Training</h1>
 <p>Welcome to this base mvc project.</p>
+
+<ul class="exercise-container">
+<?php 
+foreach($exercises as $exercise) {
+    echo "<li><p class='exercise__item'>" . $exercise->name ."</p> </li>" . '';
+}
+?>
+
+</ul>
